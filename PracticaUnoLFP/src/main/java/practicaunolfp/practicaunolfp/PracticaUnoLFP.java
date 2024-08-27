9802f54a020803f9b1c2228e8be1428dadce61a7
Javier Mérida <javiermerida202230638@cunoc.edu.gt>
2024-08-27 00:07:55 -0600

    Se agrego el proyecto de netbeans al repositorio

---
+++ PracticaUnoLFP/src/main/java/practicaunolfp/practicaunolfp/PracticaUnoLFP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package practicaunolfp.practicaunolfp;
+
+/**
+ *
+ * @author xavi
+ */
+public class PracticaUnoLFP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
