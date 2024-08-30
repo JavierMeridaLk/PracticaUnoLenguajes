180641aa1e71956d352be9570d08b8d0705afece
Javier Mérida <javiermerida202230638@cunoc.edu.gt>
2024-08-30 03:24:19 -0600

    Limpieza de codigo 1

--- PracticaUnoLFP/src/main/java/Backen/Analizador.java
+++ PracticaUnoLFP/src/main/java/Backen/Analizador.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Backen;
 
 import java.awt.Color;
@@ -18,14 +14,10 @@ import javax.swing.JTextPane;
 import javax.swing.JToggleButton;
 import javax.swing.border.Border;
 
-/**
- *
- * @author xavi
- */
 public class Analizador {
     
-     private String nombreDelArchivo;
-     
+    private String nombreDelArchivo;
+        
     public Analizador(){
         
     }
@@ -38,80 +30,85 @@ public class Analizador {
             String[] palabras = textoSinEspacios.split("(?<=\\b)(?=[(){}\\[\\],])|(?<=[(){}\\[\\],])|(?<=\\d)(?=\\D)|(?<=\\d\\.\\d+)(?=\\D)|(?<=\\d\\.\\d+)|\\s+");
 
             int cantidadDePalabras = palabras.length;
-               /*
-                // Mostrar el número total de palabras
-                System.out.println("Número total de palabras: " + cantidadDePalabras);
-                // Mostrar cada palabra individualmente
-                for (int i = 0; i < palabras.length; i++) {
-                    System.out.println("Palabra " + (i + 1) + ": " + palabras[i]);
-                }
-                */
                
             if (cantidadDeToken>=cantidadDePalabras) {
+                //realiza el analicis si la cantidad de token son igual o mayores a la cantidad de palabras ingresadas
                 for (int i = 0; i < palabras.length; i++) {
+                    
                     Token token = new Token();
                     panel.add(token.nuevoToken(palabras[i]));
-                    panel.revalidate(); // Reorganiza los componentes
-                    panel.repaint();    // Redibuja el panel
-                    
+                    panel.revalidate();
+                    panel.repaint();   
                 }
 
                 if (cantidadDePalabras < cantidadDeToken ) {
+                    //rellenar de label vacias si las palabras son menores a la cantidad de token
                     for (int i = 0; i < (cantidadDeToken-cantidadDePalabras); i++) {
+                        
                         JLabel label = new JLabel();
-                        Border border = BorderFactory.createLineBorder(Color.BLACK, 1); // Borde negro de 2 píxeles
+                        Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
                         label.setBorder(border);
                         panel.add(label);
-                        panel.revalidate(); // Reorganiza los componentes
+                        panel.revalidate();
                         panel.repaint();  
                         
                     }
                 }
+                
                 boton.setEnabled(true);
+                
             }else{
+                
                 JOptionPane.showMessageDialog(null, "La cantidad de tokens ingresados superan la cuadricula ingresada por: " + (cantidadDePalabras-cantidadDeToken),
                         "Error", JOptionPane.ERROR_MESSAGE);
-            return;
-            }
-            
-            
+                return;
+            }     
         } else {
+            
             JOptionPane.showMessageDialog(null, "El codigo fuente esta vacio.", "Error", JOptionPane.ERROR_MESSAGE);
             return;
         }  
     }
+    
     public File abrirChose(){
-        
-        
+        //abrir el chose y guardar el archivo para leerlo en otro metodo
         JFileChooser fileChooser = new JFileChooser();
          
         int seleccion = fileChooser.showOpenDialog(null);
     
         if (seleccion == JFileChooser.APPROVE_OPTION) {
+            
             File archivo = fileChooser.getSelectedFile();
-
             nombreDelArchivo= archivo.getName();
-        
             return archivo;
         } else {
+            
             JOptionPane.showMessageDialog(null, "No se seleccionó ningún archivo.", "Advertencia", JOptionPane.WARNING_MESSAGE);
         }
     
         return null;
-    
     }
+    
     public void leer(JTextPane text){
+        //usa un bufferReader para leer el texto e insertarlo en panelText
         File archivo = abrirChose();
+        
         try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
-                String line;
-                StringBuilder content = new StringBuilder();
-                while ((line = reader.readLine()) != null) {
-                    content.append(line).append("\n");
-                }
-                text.setText(content.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-                JOptionPane.showMessageDialog(null, "Error al leer el archivo: ", "Error", JOptionPane.ERROR_MESSAGE);
+            
+            String line;
+            StringBuilder content = new StringBuilder();
+            
+            while ((line = reader.readLine()) != null) {
+                
+                content.append(line).append("\n");
             }
+            
+            text.setText(content.toString());
+            
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al leer el archivo: ", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
--- PracticaUnoLFP/src/main/java/Backen/Imagen.java
+++ PracticaUnoLFP/src/main/java/Backen/Imagen.java
@@ -1,137 +1,108 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Backen;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
 
-/**
- *
- * @author xavi
- */
 public class Imagen {
-    
     
     public Imagen(){
         
     }
     
-   public int[] crearCuadricula(JPanel panel) {
-    int[] tamaños = new int[2]; // Tamaños para filas y columnas
-    
-    // Solicitar el número de filas
-    String filas = JOptionPane.showInputDialog(null, 
-            "Ingrese el número de filas:", "Tamaño de la cuadrícula", 
-            JOptionPane.QUESTION_MESSAGE);
+    public int[] crearCuadricula(JPanel panel) {
+        
+        int[] tamaños = new int[2]; //arreglo para regresar al fronted el tamaño de la cuadricula
+        // Solicitar el número de filas
+        String filas = JOptionPane.showInputDialog(null, 
+                                                    "Ingrese el número de filas:", "Tamaño de la cuadrícula", 
+                                                    JOptionPane.QUESTION_MESSAGE);
+        // Verificar si el usuario presionó "Cancelar" o dejó el cuadro de texto vacío
+        if (filas == null || filas.trim().isEmpty()) {
+            
+            JOptionPane.showMessageDialog(null, "Operación cancelada o entrada vacía.", "Error", JOptionPane.ERROR_MESSAGE);
+            tamaños[0] = 0; // Indica error
+            tamaños[1] = 0; // Indica error
+            return tamaños;
+        }
 
-    // Verificar si el usuario presionó "Cancelar" o dejó el cuadro de texto vacío
-    if (filas == null || filas.trim().isEmpty()) {
-        JOptionPane.showMessageDialog(null, "Operación cancelada o entrada vacía.", "Error", JOptionPane.ERROR_MESSAGE);
-        tamaños[0] = 0; // Indica error
-        tamaños[1] = 0; // Indica error
+        int filasInt=0;
+        
+        try {
+            
+            filasInt = Integer.parseInt(filas);
+        } catch (NumberFormatException e) {
+            
+            JOptionPane.showMessageDialog(null, "Por favor, ingrese un número válido para las filas.", "Error", JOptionPane.ERROR_MESSAGE);
+            tamaños[0] = 0; // Indica error
+            tamaños[1] = 0; // Indica error
+            return tamaños;
+        }
+        // Solicitar el número de columnas
+        String columnas = JOptionPane.showInputDialog(null, 
+                                                        "Ingrese el número de columnas:", "Tamaño de la cuadrícula", 
+                                                        JOptionPane.QUESTION_MESSAGE);
+
+        // Verificar si el usuario presionó "Cancelar" o dejó el cuadro de texto vacío
+        if (columnas == null || columnas.trim().isEmpty()) {
+            
+            JOptionPane.showMessageDialog(null, "Operación cancelada o entrada vacía.", "Error", JOptionPane.ERROR_MESSAGE);
+            tamaños[0] = 0; // Indica error
+            tamaños[1] = 0; // Indica error
+            return tamaños;
+        }
+
+        int columnasInt=0;
+        
+        try {
+            
+            columnasInt = Integer.parseInt(columnas);
+        } catch (NumberFormatException e) {
+            
+            JOptionPane.showMessageDialog(null, "Por favor, ingrese un número válido para las columnas.", "Error", JOptionPane.ERROR_MESSAGE);
+            tamaños[0] = 0; // Indica error
+            tamaños[1] = 0; // Indica error
+            return tamaños;
+        }
+        // Asignar los tamaños al array
+        tamaños[0] = filasInt;
+        tamaños[1] = columnasInt;
+        // Configurar el panel con el GridLayout
+        panel.setLayout(new GridLayout(filasInt, columnasInt));
+
         return tamaños;
-    }
-
-    int filasInt=0;
-    try {
-        filasInt = Integer.parseInt(filas);
-    } catch (NumberFormatException e) {
-        JOptionPane.showMessageDialog(null, "Por favor, ingrese un número válido para las filas.", "Error", JOptionPane.ERROR_MESSAGE);
-        tamaños[0] = 0; // Indica error
-        tamaños[1] = 0; // Indica error
-        return tamaños;
-    }
-
-    // Solicitar el número de columnas
-    String columnas = JOptionPane.showInputDialog(null, 
-            "Ingrese el número de columnas:", "Tamaño de la cuadrícula", 
-            JOptionPane.QUESTION_MESSAGE);
-
-    // Verificar si el usuario presionó "Cancelar" o dejó el cuadro de texto vacío
-    if (columnas == null || columnas.trim().isEmpty()) {
-        JOptionPane.showMessageDialog(null, "Operación cancelada o entrada vacía.", "Error", JOptionPane.ERROR_MESSAGE);
-        tamaños[0] = 0; // Indica error
-        tamaños[1] = 0; // Indica error
-        return tamaños;
-    }
-
-    int columnasInt=0;
-    try {
-        columnasInt = Integer.parseInt(columnas);
-    } catch (NumberFormatException e) {
-        JOptionPane.showMessageDialog(null, "Por favor, ingrese un número válido para las columnas.", "Error", JOptionPane.ERROR_MESSAGE);
-        tamaños[0] = 0; // Indica error
-        tamaños[1] = 0; // Indica error
-        return tamaños;
-    }
-
-    // Asignar los tamaños al array
-    tamaños[0] = filasInt;
-    tamaños[1] = columnasInt;
-
-    // Configurar el panel con el GridLayout
-    panel.setLayout(new GridLayout(filasInt, columnasInt));
-    
-    System.out.println("Filas: " + filasInt);
-    System.out.println("Columnas: " + columnasInt);
-
-    /*
-    // Prueba de relleno (opcional)
-    // Relleno del panel solo de ejemplo
-    for (int i = 1; i <= filasInt * columnasInt; i++) {
-        JLabel label = new JLabel(); // Crear el JLabel
-        Border border = BorderFactory.createLineBorder(Color.BLACK, 2); // Borde negro de 2 píxeles
-        label.setBorder(border);
-
-        // Agregar el JLabel al panel
-        panel.add(label);
-    }
-    */
-
-    return tamaños;
     }
    
-   public void exportarImagen(JPanel ImgPanel){
+    public void exportarImagen(JPanel ImgPanel){
        
-       File carpeta = new File("Imagenes");
-            if (!carpeta.exists()) {
-                carpeta.mkdirs(); // Crear la carpeta y sus subcarpetas si no existen
-            }
+        File carpeta = new File("Imagenes");
+        // Crear la carpeta y sus subcarpetas si no existen 
+        if (!carpeta.exists()) {
+            carpeta.mkdirs(); 
+        }
+                 
+        BufferedImage image = new BufferedImage(ImgPanel.getWidth(), ImgPanel.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = image.createGraphics();
+        ImgPanel.paint(g2d);
+        g2d.dispose();
+        // Guardar la imagen en un archivo
+        try {
             
-       
-       BufferedImage image = new BufferedImage(ImgPanel.getWidth(), ImgPanel.getHeight(), BufferedImage.TYPE_INT_ARGB);
-                Graphics2D g2d = image.createGraphics();
-                ImgPanel.paint(g2d);
-                g2d.dispose();
-
-                // Guardar la imagen en un archivo
-                try {
-                    String nombre = JOptionPane.showInputDialog(null, 
-                    "Ingrese un nombre para la imagen:", "Nombre", 
-                    JOptionPane.QUESTION_MESSAGE);
-                    File archivo = new File(carpeta, nombre);
-                    
-                    ImageIO.write(image, "png", archivo);
-                    JOptionPane.showMessageDialog(null, "Imagen guardada como: "+ nombre + ", en la carpeta: "+ carpeta , "Exito", JOptionPane.INFORMATION_MESSAGE);
-                   
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    JOptionPane.showMessageDialog(null, "Error al guardar la imagen: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                }
-   }
-    
+            String nombre = JOptionPane.showInputDialog(null,"Ingrese un nombre para la imagen:", "Nombre", 
+            JOptionPane.QUESTION_MESSAGE);
+            File archivo = new File(carpeta, nombre);
+                 
+            ImageIO.write(image, "png", archivo);
+            JOptionPane.showMessageDialog(null, "Imagen guardada como: "+ nombre + ", en la carpeta: "+ carpeta , "Exito", JOptionPane.INFORMATION_MESSAGE);      
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al guardar la imagen", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
